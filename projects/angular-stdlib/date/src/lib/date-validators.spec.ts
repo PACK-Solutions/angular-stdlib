61035e0f2b97f1bc23394ebb0d309476c1f81c8d
magnasilvar <3359919+magnasilvar@users.noreply.github.com>
2019-12-27 22:56:21 +0100

    feat(date): add DateValidators skeleton
    see the issue for details
    
    Refs #3

---
+++ projects/angular-stdlib/date/src/lib/date-validators.spec.ts
@@ -0,0 +1,9 @@
+import { DateValidators } from './date-validators';
+
+describe('DateValidators', () => {
+
+  it('should create an instance', () => {
+    expect(new DateValidators()).toBeTruthy();
+  });
+
+});
---
+++ projects/angular-stdlib/date/src/lib/date-validators.ts
@@ -0,0 +1,23 @@
+export class DateValidators {
+
+  public static date() {
+    // TODO
+  }
+
+  public static beforeDate() {
+    // TODO
+  }
+
+  public static notBeforeDate() {
+    // TODO
+  }
+
+  public static afterDate() {
+    // TODO
+  }
+
+  public static notAfterDate() {
+    // TODO
+  }
+
+}
