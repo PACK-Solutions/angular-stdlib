5ab29d5cbeb68e7cc0a015844b9bcf00510a9432
magnasilvar <3359919+magnasilvar@users.noreply.github.com>
2020-01-02 17:32:37 +0100

    feat(date): add DateValidators for FormControl
    see the issue for details
    
    Refs #3

--- projects/angular-stdlib/date/src/lib/date-validators.spec.ts
+++ projects/angular-stdlib/date/src/lib/date-validators.spec.ts
@@ -1,9 +1,144 @@
+import { FormControl, ValidatorFn } from '@angular/forms';
 import { DateValidators } from './date-validators';
 
 describe('DateValidators', () => {
 
-  it('should create an instance', () => {
-    expect(new DateValidators()).toBeTruthy();
+  const stringFormatDate = '2019-01-01';
+  const firstDate = new Date('2018-01-01');
+  const secondDate = new Date('2020-01-01');
+
+  it('should have valid date', () => {
+    const dateControl = new FormControl(new Date(), DateValidators.date);
+    expect(dateControl.valid).toBeTruthy();
+    expect(dateControl.hasError('date')).toBeFalsy();
+  });
+
+  it('should have invalid date', () => {
+    const dateControl = new FormControl(stringFormatDate, DateValidators.date);
+    expect(dateControl.valid).toBeFalsy();
+    expect(dateControl.hasError('date')).toBeTruthy();
+  });
+
+  it('should have invalid date (all DateValidators)', () => {
+    const dateControl = new FormControl(stringFormatDate, [
+      DateValidators.date,
+      DateValidators.beforeDate(firstDate),
+      DateValidators.notBeforeDate(secondDate),
+      DateValidators.afterDate(secondDate),
+      DateValidators.notAfterDate(firstDate)
+    ]);
+    expect(dateControl.valid).toBeFalsy();
+    expect(dateControl.hasError('date')).toBeTruthy();
+    expect(dateControl.hasError('beforeDate')).toBeFalsy();
+    expect(dateControl.hasError('notBeforeDate')).toBeFalsy();
+    expect(dateControl.hasError('afterDate')).toBeFalsy();
+    expect(dateControl.hasError('notAfterDate')).toBeFalsy();
+  });
+
+  it('should be before date valid', () => {
+    expectValidDate(firstDate, DateValidators.beforeDate(secondDate), 'beforeDate');
+  });
+
+  it('should be before date valid (bad format date)', () => {
+    expectValidDate(stringFormatDate, DateValidators.beforeDate(secondDate), 'beforeDate');
+  });
+
+  it('should be before date invalid (same dates)', () => {
+    const dateControl = new FormControl(firstDate, DateValidators.beforeDate(firstDate));
+    expect(dateControl.valid).toBeFalsy();
+    expect(dateControl.hasError('beforeDate')).toBeTruthy();
+  });
+
+  it('should be before date invalid (date after)', () => {
+    const dateControl = new FormControl(secondDate, DateValidators.beforeDate(firstDate));
+    expect(dateControl.valid).toBeFalsy();
+    expect(dateControl.getError('beforeDate').dateName).toBeUndefined();
+  });
+
+  it('should be before date invalid with dateName', () => {
+    const dateControl = new FormControl(secondDate, DateValidators.beforeDate(firstDate, 'birthdate'));
+    expect(dateControl.valid).toBeFalsy();
+    expect(dateControl.getError('beforeDate').dateName).toBe('birthdate');
+  });
+
+  it('should not be before date valid', () => {
+    expectValidDate(secondDate, DateValidators.notBeforeDate(firstDate), 'notBeforeDate');
+  });
+
+  it('should not be before date valid (bad format date)', () => {
+    expectValidDate(stringFormatDate, DateValidators.notBeforeDate(secondDate), 'notBeforeDate');
+  });
+
+  it('should not be before date valid (same dates)', () => {
+    expectValidDate(firstDate, DateValidators.notBeforeDate(firstDate), 'notBeforeDate');
+  });
+
+  it('should not be before date invalid (date before)', () => {
+    const dateControl = new FormControl(firstDate, DateValidators.notBeforeDate(secondDate));
+    expect(dateControl.valid).toBeFalsy();
+    expect(dateControl.getError('notBeforeDate').dateName).toBeUndefined();
+  });
+
+  it('should not be before date invalid with dateName', () => {
+    const dateControl = new FormControl(firstDate, DateValidators.notBeforeDate(secondDate, 'birthdate'));
+    expect(dateControl.valid).toBeFalsy();
+    expect(dateControl.getError('notBeforeDate').dateName).toBe('birthdate');
+  });
+
+  it('should be after date valid', () => {
+    expectValidDate(secondDate, DateValidators.afterDate(firstDate), 'afterDate');
+  });
+
+  it('should be after date valid (bad format date)', () => {
+    expectValidDate(stringFormatDate, DateValidators.afterDate(secondDate), 'afterDate');
+  });
+
+  it('should be after date invalid (same dates)', () => {
+    const dateControl = new FormControl(firstDate, DateValidators.afterDate(firstDate));
+    expect(dateControl.valid).toBeFalsy();
+    expect(dateControl.hasError('afterDate')).toBeTruthy();
+  });
+
+  it('should be after date invalid (date before)', () => {
+    const dateControl = new FormControl(firstDate, DateValidators.afterDate(secondDate));
+    expect(dateControl.valid).toBeFalsy();
+    expect(dateControl.getError('afterDate').dateName).toBeUndefined();
+  });
+
+  it('should be after date invalid with dateName', () => {
+    const dateControl = new FormControl(firstDate, DateValidators.afterDate(secondDate, 'birthdate'));
+    expect(dateControl.valid).toBeFalsy();
+    expect(dateControl.getError('afterDate').dateName).toBe('birthdate');
+  });
+
+  it('should not be after date valid', () => {
+    expectValidDate(firstDate, DateValidators.notAfterDate(secondDate), 'notAfterDate');
+  });
+
+  it('should not be after date valid (bad format date)', () => {
+    expectValidDate(stringFormatDate, DateValidators.notAfterDate(secondDate), 'notAfterDate');
+  });
+
+  it('should not be after date valid (same dates)', () => {
+    expectValidDate(firstDate, DateValidators.notAfterDate(firstDate), 'notAfterDate');
+  });
+
+  it('should not be after date invalid (date after)', () => {
+    const dateControl = new FormControl(secondDate, DateValidators.notAfterDate(firstDate));
+    expect(dateControl.valid).toBeFalsy();
+    expect(dateControl.getError('notAfterDate').dateName).toBeUndefined();
+  });
+
+  it('should not be after date invalid with dateName', () => {
+    const dateControl = new FormControl(secondDate, DateValidators.notAfterDate(firstDate, 'birthdate'));
+    expect(dateControl.valid).toBeFalsy();
+    expect(dateControl.getError('notAfterDate').dateName).toBe('birthdate');
   });
 
 });
+
+function expectValidDate(value: any, validator: ValidatorFn, errorCode: string) {
+  const dateControl = new FormControl(value, validator);
+  expect(dateControl.valid).toBeTruthy();
+  expect(dateControl.hasError(errorCode)).toBeFalsy();
+}
--- projects/angular-stdlib/date/src/lib/date-validators.ts
+++ projects/angular-stdlib/date/src/lib/date-validators.ts
@@ -1,23 +1,153 @@
-export class DateValidators {
+import { AbstractControl, ValidatorFn, ValidationErrors } from '@angular/forms';
 
-  public static date() {
-    // TODO
-  }
-
-  public static beforeDate() {
-    // TODO
-  }
-
-  public static notBeforeDate() {
-    // TODO
-  }
-
-  public static afterDate() {
-    // TODO
-  }
-
-  public static notAfterDate() {
-    // TODO
-  }
-
+function isEmptyOrDate(value: any): boolean {
+  return value == null || value instanceof Date;
 }
+
+function isValid(value: Date, dateParam: Date, operator): boolean {
+  if (value == null || !(value instanceof Date) || dateParam == null || operator(value, dateParam)) {
+    return true;
+  }
+  return false;
+}
+
+function buildErrorValue(dateName?: string): true | { dateName: string } {
+  return dateName ? { dateName } : true;
+}
+
+function date(control: AbstractControl): ValidationErrors | null {
+  if (isEmptyOrDate(control.value)) {
+    return null;
+  }
+  return { date: true };
+}
+
+function beforeDate(dateParam: Date, dateName?: string): ValidatorFn {
+  return (control: AbstractControl) => {
+    return isValid(control.value, dateParam, (a, b) => a < b) ? null : { beforeDate: buildErrorValue(dateName) };
+  };
+}
+
+function notBeforeDate(dateParam: Date, dateName?: string): ValidatorFn {
+  return (control: AbstractControl) => {
+    return isValid(control.value, dateParam, (a, b) => a >= b) ? null : { notBeforeDate: buildErrorValue(dateName) };
+  };
+}
+
+function afterDate(dateParam: Date, dateName?: string): ValidatorFn {
+  return (control: AbstractControl) => {
+    return isValid(control.value, dateParam, (a, b) => a > b) ? null : { afterDate: buildErrorValue(dateName) };
+  };
+}
+
+function notAfterDate(dateParam: Date, dateName?: string): ValidatorFn {
+  return (control: AbstractControl) => {
+    return isValid(control.value, dateParam, (a, b) => a <= b) ? null : { notAfterDate: buildErrorValue(dateName) };
+  };
+}
+
+/**
+ * @description
+ * Provides a set of built-in validators for Dates that can be used by form controls.
+ *
+ * A validator is a function that processes a `FormControl` or collection of
+ * controls and returns an error map or null. A null map means that validation has passed.
+ *
+ * @publicApi
+ */
+export const DateValidators = {
+  /**
+   * @description
+   * Validator that requires the control's value to be a `Date`
+   *
+   * @usageNotes Usage :
+   *
+   * ```typescript
+   * const control = new FormControl(123, DateValidators.date);
+   * console.log(control.errors); // {date: true}
+   * ```
+   *
+   * @returns An error map with the `date` property if the validation check fails, otherwise `null`.
+   */
+  date,
+
+  /**
+   * @description
+   * Validator that requires the control's value to be prior than the date in parameter.
+   *
+   * @usageNotes Usage :
+   * ```typescript
+   * const control = new FormControl(new Date('2020-01-01'), DateValidators.beforeDate(new Date('2019-01-01')));
+   * console.log(control.errors); // {beforeDate: true}
+   * ```
+   * With optional `dateName` parameter :
+   * ```typescript
+   * const control = new FormControl(new Date('2020-01-01'), DateValidators.beforeDate(new Date('2019-01-01'), 'birthdate'));
+   * console.log(control.errors); // {beforeDate: {dateName: 'birthdate'}}
+   * ```
+   *
+   * @returns A validator function that returns an error map with the
+   * `beforeDate` property if the validation check fails, otherwise `null`.
+   */
+  beforeDate,
+
+  /**
+   * @description
+   * Validator that requires the control's value not to be prior than the date in parameter.
+   *
+   * @usageNotes Usage :
+   * ```typescript
+   * const control = new FormControl(new Date('2019-01-01'), DateValidators.notBeforeDate(new Date('2020-01-01')));
+   * console.log(control.errors); // {notBeforeDate: true}
+   * ```
+   * With optional `dateName` parameter :
+   * ```typescript
+   * const control = new FormControl(new Date('2019-01-01'), DateValidators.notBeforeDate(new Date('2020-01-01'), 'birthdate'));
+   * console.log(control.errors); // {notBeforeDate: {dateName: 'birthdate'}}
+   * ```
+   *
+   * @returns A validator function that returns an error map with the
+   * `notBeforeDate` property if the validation check fails, otherwise `null`.
+   */
+  notBeforeDate,
+
+  /**
+   * @description
+   * Validator that requires the control's value to be later than the date in parameter.
+   *
+   * @usageNotes Usage :
+   * ```typescript
+   * const control = new FormControl(new Date('2019-01-01'), DateValidators.afterDate(new Date('2020-01-01')));
+   * console.log(control.errors); // {afterDate: true}
+   * ```
+   * With optional `dateName` parameter :
+   * ```typescript
+   * const control = new FormControl(new Date('2019-01-01'), DateValidators.afterDate(new Date('2020-01-01'), 'birthdate'));
+   * console.log(control.errors); // {afterDate: {dateName: 'birthdate'}}
+   * ```
+   *
+   * @returns A validator function that returns an error map with the
+   * `afterDate` property if the validation check fails, otherwise `null`.
+   */
+  afterDate,
+
+  /**
+   * @description
+   * Validator that requires the control's value not to be later than the date in parameter.
+   *
+   * @usageNotes Usage :
+   * ```typescript
+   * const control = new FormControl(new Date('2020-01-01'), DateValidators.notAfterDate(new Date('2019-01-01')));
+   * console.log(control.errors); // {notAfterDate: true}
+   * ```
+   * With optional `dateName` parameter :
+   * ```typescript
+   * const control = new FormControl(new Date('2020-01-01'), DateValidators.notAfterDate(new Date('2019-01-01'), 'birthdate'));
+   * console.log(control.errors); // {notAfterDate: {dateName: 'birthdate'}}
+   * ```
+   *
+   * @returns A validator function that returns an error map with the
+   * `notAfterDate` property if the validation check fails, otherwise `null`.
+   */
+  notAfterDate
+};
